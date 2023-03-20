f8864cbe16f1ff5dc8e16a7f4490815fadd90e32
iNavin <i.navinkumar1998@gmail.com>
2023-03-21 01:58:56 +0530

    corrected the errors

---
+++ ble/_build/nrf52833_xxaa/utf.c.d
@@ -0,0 +1,20 @@
+_build/nrf52833_xxaa/utf.c.o: \
+ ../nRF5_SDK_17.1.0_ddde560/external/utf_converter/utf.c \
+ ../nRF5_SDK_17.1.0_ddde560/external/utf_converter/utf.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h
+../nRF5_SDK_17.1.0_ddde560/external/utf_converter/utf.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/stdint.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/features.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h:
+/opt/arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h:
